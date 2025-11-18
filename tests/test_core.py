0ea78638fc9fa84672dc48802d20e2324252c0ba
Kooroush Masoumi <kooroushmasoumi@gmail.com>
2025-11-18 20:13:02 +0330

    test streaming backpressure pt2

--- tests/test_core.py
+++ tests/test_core.py
@@ -78,3 +78,4 @@ def test_select_star_risk_flagged(analyzer):
 def test_parse_failure_raises(analyzer):
     with pytest.raises(AnalyzerError):
         analyzer.analyze("THIS IS NOT SQL AT ALL !!!")
+
